c0859dac43753446b666fc31a6ea93e3ddc26113
s-doris <swdoriz@gmail.com>
2021-03-16 02:14:03 +0900

    init :)

---
+++ DrinkSomeWater/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrinkSomeWater
+//
+//  Created by once on 2021/03/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
