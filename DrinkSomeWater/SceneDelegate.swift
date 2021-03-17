3980abca7cc4aaeb5af0a95d02637029a3a39ab8
s-doris <swdoriz@gmail.com>
2021-03-17 15:02:10 +0900

    feat : rootVC 수정

--- DrinkSomeWater/SceneDelegate.swift
+++ DrinkSomeWater/SceneDelegate.swift
@@ -15,9 +15,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        let view = MainViewController()
-        view.reactor = MainViewReactor()
-        window?.rootViewController = view
+        let viewController = MainViewController(reactor: MainViewReactor())
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
         window?.windowScene = scene
         window?.makeKeyAndVisible()
         guard let _ = (scene as? UIWindowScene) else { return }
