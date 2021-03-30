2b0ebd8bf85b647498ef01296cd02e0131a818d2
s-doris <swdoriz@gmail.com>
2021-03-30 20:27:24 +0900

    feat : fetchGoal water init

--- DrinkSomeWater/Sources/Services/WaterService.swift
+++ DrinkSomeWater/Sources/Services/WaterService.swift
@@ -15,6 +15,7 @@ enum WaterEvent {
 protocol WaterServiceProtocol {
   var event: PublishSubject<WaterEvent> { get }
   func fetchWater() -> Observable<Float>
+  func fetchGoal() -> Observable<Int>
   
   @discardableResult
   func updateWater(to ml: Float) -> Observable<Float>
@@ -26,6 +27,13 @@ final class WaterService: BaseService, WaterServiceProtocol {
   func fetchWater() -> Observable<Float> {
     let currentWater: Float = 500
     return .just(currentWater)
+  }
+  
+  func fetchGoal() -> Observable<Int> {
+    guard let goalWater = self.provider.userDefaultsService.value(forkey: .goal) else {
+      return .empty()
+    }
+    return .just(goalWater)
   }
   
   @discardableResult
