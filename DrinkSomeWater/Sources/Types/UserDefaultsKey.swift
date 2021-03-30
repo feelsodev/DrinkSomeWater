c7296350f3ec79c9be7acc20d539cffaaf6bd680
s-doris <swdoriz@gmail.com>
2021-03-30 19:38:26 +0900

    feat : UserDefaultsKey String Literal extension init

--- DrinkSomeWater/Sources/Types/UserDefaultsKey.swift
+++ DrinkSomeWater/Sources/Types/UserDefaultsKey.swift
@@ -11,3 +11,17 @@ struct UserDefaultsKey<T> {
   typealias Key<T> = UserDefaultsKey<T>
   let key: String
 }
+
+extension UserDefaultsKey: ExpressibleByStringLiteral {
+  public init(unicodeScalarLiteral value: StringLiteralType) {
+    self.init(key: value)
+  }
+
+  public init(extendedGraphemeClusterLiteral value: StringLiteralType) {
+    self.init(key: value)
+  }
+
+  public init(stringLiteral value: StringLiteralType) {
+    self.init(key: value)
+  }
+}
