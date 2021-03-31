771ffc9a8601dc1f798bd1919d1bad6f895a6830
s-doris <swdoriz@gmail.com>
2021-03-31 22:06:14 +0900

    feat : WaterRecord model init

--- DrinkSomeWater/Sources/Models/WaterRecord.swift
+++ DrinkSomeWater/Sources/Models/WaterRecord.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct WaterRecord {
+  var date: Date
+  var value: Int
+  
+  init(date: Date, value: Int) {
+    self.date = date
+    self.value = value
+  }
+  
+  init?(dictionary: [String: Any]) {
+    self.date = dictionary["date"] as! Date
+    self.value = dictionary["value"] as! Int
+  }
+  
+  func asDictionary() -> [String: Any] {
+    let dictionary: [String: Any] = [
+      "date": self.date,
+      "value": self.value,
+    ]
+    return dictionary
+  }
+}
