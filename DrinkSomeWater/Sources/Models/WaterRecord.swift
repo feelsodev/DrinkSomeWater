42571eb451736043d6b061a183a51109f16cfb20
s-doris <swdoriz@gmail.com>
2021-04-01 23:45:55 +0900

    feat : WaterRecord model 수정

--- DrinkSomeWater/Sources/Models/WaterRecord.swift
+++ DrinkSomeWater/Sources/Models/WaterRecord.swift
@@ -10,21 +10,29 @@ import Foundation
 struct WaterRecord: ModelType, Identifiable {
   var date: Date
   var value: Int
+  var isSuccess: Bool
+  var goal: Int
   
-  init(date: Date, value: Int) {
+  init(date: Date, value: Int, isSuccess: Bool, goal: Int) {
     self.date = date
     self.value = value
+    self.isSuccess = isSuccess
+    self.goal = goal
   }
   
   init?(dictionary: [String: Any]) {
     self.date = dictionary["date"] as! Date
     self.value = dictionary["value"] as! Int
+    self.isSuccess = dictionary["isSuccess"] as! Bool
+    self.goal = dictionary["goal"] as! Int
   }
   
   func asDictionary() -> [String: Any] {
     let dictionary: [String: Any] = [
       "date": self.date,
       "value": self.value,
+      "isSuccess": self.isSuccess,
+      "goal": self.goal
     ]
     return dictionary
   }
