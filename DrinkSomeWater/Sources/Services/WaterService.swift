d3c4795707b4a0980d3078da990e324943926d31
s-doris <swdoriz@gmail.com>
2021-04-01 15:38:00 +0900

    feat : WaterService UserDefault 연동하여 기능 적용

--- DrinkSomeWater/Sources/Services/WaterService.swift
+++ DrinkSomeWater/Sources/Services/WaterService.swift
@@ -9,18 +9,21 @@ import Foundation
 import RxSwift
 
 enum WaterEvent {
-  case updateWater(Float)
+  case updateWater([WaterRecord])
   case updateGoal(Int)
 }
 
 protocol WaterServiceProtocol {
   var event: PublishSubject<WaterEvent> { get }
-  func fetchWater() -> Observable<Float>
+  func fetchWater() -> Observable<[WaterRecord]>
   func fetchGoal() -> Observable<Int>
   
   @discardableResult
-  func updateWater(to ml: Float) -> Observable<Float>
+  func saveWater(_ waterRecord: [WaterRecord]) -> Observable<Void>
   
+  @discardableResult
+  func updateWater(to ml: Float) -> Observable<[WaterRecord]>
+
   @discardableResult
   func updateGoal(to ml: Int) -> Observable<Int>
 }
@@ -28,10 +31,20 @@ protocol WaterServiceProtocol {
 final class WaterService: BaseService, WaterServiceProtocol {
   let event = PublishSubject<WaterEvent>()
   
-  func fetchWater() -> Observable<Float> {
-    guard let currentValue = self.provider.userDefaultsService.value(forkey: .current) else { return .just(0)
+  func fetchWater() -> Observable<[WaterRecord]> {
+    if let currentValue = self.provider.userDefaultsService.value(forkey: .current) {
+      var water = currentValue.compactMap(WaterRecord.init)
+      if !water.contains(where: { $0.date.checkToday() }) {
+        water.append(WaterRecord(date: Date(), value: 0))
+      }
+      return .just(water)
     }
-    return .just(Float(currentValue))
+    
+    // 값이 존재하지 않을경우 즉, 제일 초기 앱 실행시
+    let waterRecord = WaterRecord(date: Date(), value: 0)
+    let value = waterRecord.asDictionary()
+    self.provider.userDefaultsService.set(value: [value], forkey: .current)
+    return .just([waterRecord])
   }
   
   func fetchGoal() -> Observable<Int> {
@@ -41,20 +54,32 @@ final class WaterService: BaseService, WaterServiceProtocol {
     return .just(goalWater)
   }
   
+  func saveWater(_ waterRecord: [WaterRecord]) -> Observable<Void> {
+    let dicts = waterRecord.map { $0.asDictionary() }
+    self.provider.userDefaultsService.set(value: dicts, forkey: .current)
+    return .just(Void())
+  }
+  
   @discardableResult
-  func updateWater(to ml: Float) -> Observable<Float> {
-    if let currentValue = self.provider.userDefaultsService.value(forkey: .current) {
-      self.provider.userDefaultsService.set(value: currentValue + Int(ml), forkey: .current)
-      
-      // 초기화 위한 슈가 코드
-//      self.provider.userDefaultsService.set(value: 0, forkey: .current)
-    }
-    
+  func updateWater(to ml: Float) -> Observable<[WaterRecord]> {
     return self.fetchWater()
-      .map { $0 }
-      .do(onNext: { water in
-        self.event.onNext(.updateWater(water))
-      })
+      .flatMap { [weak self] waterRecord -> Observable<[WaterRecord]> in
+        guard let `self` = self else { return .empty() }
+        guard let index = waterRecord.firstIndex(where: { $0.date.checkToday() }) else {
+          return .empty()
+        }
+        var waterRecord = waterRecord
+        let newRecord = waterRecord[index].with {
+          $0.value += Int(ml)
+          $0.date = Date()
+        }
+        waterRecord[index] = newRecord
+        return self.saveWater(waterRecord)
+          .map { waterRecord }
+          .do { waterRecordList in
+            self.event.onNext(.updateWater(waterRecordList))
+          }
+      }
   }
   
   @discardableResult
