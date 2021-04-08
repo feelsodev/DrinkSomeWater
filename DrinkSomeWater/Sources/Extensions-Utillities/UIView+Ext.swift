944c5d788f59d898d0f76841c4f17f865ba4c413
s-doris <swdoriz@gmail.com>
2021-04-09 02:55:34 +0900

    feat : UIView fade animation init

--- DrinkSomeWater/Sources/Extensions-Utillities/UIView+Ext.swift
+++ DrinkSomeWater/Sources/Extensions-Utillities/UIView+Ext.swift
@@ -5,4 +5,29 @@
 //  Created by once on 2021/04/09.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+  func fadeIn(_ duration: TimeInterval? = 0.2, onCompletion: (() -> Void)? = nil) {
+    self.alpha = 0
+    self.isHidden = false
+    UIView.animate(
+      withDuration: duration!,
+      animations: { self.alpha = 1 },
+      completion: { _ in
+        if let complete = onCompletion { complete() }
+      }
+    )
+  }
+  
+  func fadeOut(_ duration: TimeInterval? = 0.2, onCompletion: (() -> Void)? = nil) {
+    UIView.animate(
+      withDuration: duration!,
+      animations: { self.alpha = 0 },
+      completion: { _ in
+        self.isHidden = true
+        if let complete = onCompletion { complete() }
+      }
+    )
+  }
+}
