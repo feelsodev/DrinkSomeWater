2f11505b86477e76cba768414feb4b1409857878
s-doris <swdoriz@gmail.com>
2021-04-13 23:53:55 +0900

    feat : info View init

--- DrinkSomeWater/Sources/ViewController/Information/InformationViewController.swift
+++ DrinkSomeWater/Sources/ViewController/Information/InformationViewController.swift
@@ -5,4 +5,21 @@
 //  Created by once on 2021/04/13.
 //
 
-import Foundation
+import UIKit
+import WaveAnimationView
+
+class InformationViewController: BaseViewController {
+  let backgroundView = UIView().then {
+    $0.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+  }
+  
+  override func setupConstraints() {
+    self.view.backgroundColor = .white
+    self.view.addSubview(self.backgroundView)
+    self.backgroundView.snp.makeConstraints {
+      $0.top.leading.trailing.equalToSuperview()
+      $0.height.equalTo(100)
+    }
+  }
+  
+}
