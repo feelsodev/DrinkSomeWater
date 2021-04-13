ed626c16b7adc2c40a9e20f999fc269fd9acd72f
s-doris <swdoriz@gmail.com>
2021-04-14 01:38:07 +0900

    feat : InfoCell init

--- DrinkSomeWater/Sources/ViewController/Information/InfoCell.swift
+++ DrinkSomeWater/Sources/ViewController/Information/InfoCell.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import ReactorKit
+import RxSwift
+
+final class InfoCell: BaseTableViewCell, View {
+  typealias Reactor = InfoCellReactor
+  
+  let icon = UIImageView().then {
+    $0.backgroundColor = .white
+  }
+  let titleLabel = UILabel().then {
+    $0.textColor = .black
+    $0.numberOfLines = 0
+  }
+  
+  func bind(reactor: Reactor) {
+    self.titleLabel.text = reactor.currentState.title
+  }
+  
+  override func initialize() {
+    [self.icon, self.titleLabel].forEach { self.contentView.addSubview($0) }
+    self.icon.snp.makeConstraints {
+      $0.leading.equalToSuperview().offset(10)
+      $0.centerY.equalToSuperview()
+      $0.height.width.equalTo(40)
+    }
+    self.titleLabel.snp.makeConstraints {
+      $0.leading.equalTo(self.icon.snp.trailing).offset(10)
+      $0.centerY.equalToSuperview()
+    }
+  }
+}
