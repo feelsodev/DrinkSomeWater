a8559f3390e7f7424d5860f7677053e76e2539d2
s-doris <swdoriz@gmail.com>
2021-04-14 02:05:42 +0900

    feat : InformationViewController layer view setting

--- DrinkSomeWater/Sources/ViewController/Information/InformationViewController.swift
+++ DrinkSomeWater/Sources/ViewController/Information/InformationViewController.swift
@@ -16,6 +16,13 @@ final class InformationViewController: BaseViewController, View {
     $0.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
   }
   
+  let tableView = UITableView().then {
+    $0.backgroundColor = .lightGray
+    $0.register(InfoCell.self, forCellReuseIdentifier: "cell")
+    $0.layer.cornerRadius = 20
+    $0.layer.masksToBounds = true
+    $0.separatorColor = .clear
+  }
   
   // MARK: - Initialize
   
@@ -35,10 +42,17 @@ final class InformationViewController: BaseViewController, View {
   override func setupConstraints() {
     self.view.backgroundColor = .white
     self.view.addSubview(self.backgroundView)
+    self.view.addSubview(self.tableView)
+    self.view.bringSubviewToFront(self.tableView)
     self.backgroundView.snp.makeConstraints {
       $0.top.leading.trailing.equalToSuperview()
-      $0.height.equalTo(100)
+      $0.height.equalTo(UIScreen.main.bounds.height / 4)
     }
-  }
-  
+    self.tableView.snp.makeConstraints {
+      $0.top.equalTo(self.view.snp.top).offset(UIScreen.main.bounds.height / 4 - 40)
+      $0.leading.equalToSuperview().offset(15)
+      $0.trailing.equalToSuperview().offset(-15)
+      $0.height.equalTo(UIScreen.main.bounds.height / 3 * 2)
+    }
+  } 
 }
