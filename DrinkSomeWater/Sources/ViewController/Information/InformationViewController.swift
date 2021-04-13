cd253dcfd6fb64acc0b0b2ae72c3ced8f2947874
s-doris <swdoriz@gmail.com>
2021-04-14 03:03:15 +0900

    feat : dismiss animation init

--- DrinkSomeWater/Sources/ViewController/Information/InformationViewController.swift
+++ DrinkSomeWater/Sources/ViewController/Information/InformationViewController.swift
@@ -88,6 +88,12 @@ final class InformationViewController: BaseViewController, View {
       .distinctUntilChanged()
       .subscribe { [weak self] _ in
         guard let `self` = self else { return }
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromLeft
+        self.view.window?.layer.add(transition, forKey: nil)
         self.dismiss(animated: true, completion: nil)
       }
       .disposed(by: self.disposeBag)
