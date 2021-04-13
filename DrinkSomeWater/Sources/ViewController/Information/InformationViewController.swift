56ec1ce0fd190038602faaa33ee62a2e519699c3
s-doris <swdoriz@gmail.com>
2021-04-14 00:17:44 +0900

    feat : InformationView init

--- DrinkSomeWater/Sources/ViewController/Information/InformationViewController.swift
+++ DrinkSomeWater/Sources/ViewController/Information/InformationViewController.swift
@@ -6,11 +6,30 @@
 //
 
 import UIKit
+import ReactorKit
+import RxCocoa
+import RxSwift
 import WaveAnimationView
 
-class InformationViewController: BaseViewController {
+final class InformationViewController: BaseViewController, View {
   let backgroundView = UIView().then {
     $0.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+  }
+  
+  
+  // MARK: - Initialize
+  
+  init(reactor: InformationViewReactor) {
+    super.init()
+    self.reactor = reactor
+  }
+  
+  required convenience init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func bind(reactor: InformationViewReactor) {
+    
   }
   
   override func setupConstraints() {
---
+++ DrinkSomeWater/Sources/ViewController/Information/InformationViewReactor.swift
@@ -0,0 +1,8 @@
+//
+//  InformationViewReactor.swift
+//  DrinkSomeWater
+//
+//  Created by once on 2021/04/14.
+//
+
+import Foundation
