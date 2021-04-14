068280a3cea3a51d16dedab7d85d05f2d11d4688
s-doris <swdoriz@gmail.com>
2021-04-14 21:31:26 +0900

    feat : override point update

--- DrinkSomeWater/Sources/ViewController/BaseComponent/BaseTableViewCell.swift
+++ DrinkSomeWater/Sources/ViewController/BaseComponent/BaseTableViewCell.swift
@@ -20,14 +20,18 @@ class BaseTableViewCell: UITableViewCell {
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
     self.initialize()
+    self.setupConstraints()
   }
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  // Override point
   func initialize() {
-    
+    // Override point
+  }
+  
+  func setupConstraints() {
+    // Override point
   }
 }
