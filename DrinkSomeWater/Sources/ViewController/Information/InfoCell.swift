d33dd75181af957a784cb1231e72c4ebc0d8f255
s-doris <swdoriz@gmail.com>
2021-04-14 21:27:02 +0900

    feat : InfoCell accessoryView init

--- DrinkSomeWater/Sources/ViewController/Information/InfoCell.swift
+++ DrinkSomeWater/Sources/ViewController/Information/InfoCell.swift
@@ -26,9 +26,36 @@ final class InfoCell: BaseTableViewCell, View {
   func bind(reactor: Reactor) {
     self.titleLabel.text = reactor.currentState.title
     self.icon.image = reactor.currentState.key.getImage()
+    
+    switch reactor.currentState.key {
+    case .alarm:
+      let switchView = UISwitch().then {
+        $0.isOn = true
+        $0.isUserInteractionEnabled = true
+      }
+      self.accessoryView = switchView
+    case .review:
+      self.accessoryType = .disclosureIndicator
+    case .version:
+      let versionLabel = UILabel(
+        frame: CGRect(x: 0, y: 0, width: 42, height: 21)
+      ).then {
+        $0.text = "1.2.1"
+        $0.textColor = .black
+      }
+      self.accessoryView = versionLabel
+    case .question:
+      self.accessoryType = .disclosureIndicator
+    case .license:
+      self.accessoryType = .disclosureIndicator
+    }
   }
   
   override func initialize() {
+    self.backgroundColor = .white
+  }
+  
+  override func setupConstraints() {
     [self.icon, self.titleLabel].forEach { self.contentView.addSubview($0) }
     self.icon.snp.makeConstraints {
       $0.leading.equalToSuperview().offset(10)
@@ -37,6 +64,7 @@ final class InfoCell: BaseTableViewCell, View {
     }
     self.titleLabel.snp.makeConstraints {
       $0.leading.equalTo(self.icon.snp.trailing).offset(10)
+      $0.trailing.equalToSuperview().offset(-30)
       $0.centerY.equalToSuperview()
     }
   }
