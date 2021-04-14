be365ad89b35ebbd1b4d1189110b1bbb42bc5586
s-doris <swdoriz@gmail.com>
2021-04-14 19:43:26 +0900

    chore : 라인정리 및 주석처리

--- DrinkSomeWater/Sources/ViewController/BaseComponent/BaseTableViewCell.swift
+++ DrinkSomeWater/Sources/ViewController/BaseComponent/BaseTableViewCell.swift
@@ -9,19 +9,19 @@ import Foundation
 import RxSwift
 
 class BaseTableViewCell: UITableViewCell {
-
+  
   // MARK: Properties
-
+  
   var disposeBag: DisposeBag = DisposeBag()
-
-
+  
+  
   // MARK: Initializing
-
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
     self.initialize()
   }
-
+  
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
