90165bdc8fd1970e0a4147f14fc33c7b3e8c3e4a
s-doris <swdoriz@gmail.com>
2021-04-14 14:35:34 +0900

    feat : InfoKey 를 통한 system image init

--- DrinkSomeWater/Sources/Models/Info.swift
+++ DrinkSomeWater/Sources/Models/Info.swift
@@ -5,8 +5,33 @@
 //  Created by once on 2021/04/14.
 //
 
-import Foundation
+import UIKit
+
+enum InfoKey {
+  case alarm
+  case review
+  case version
+  case license
+  
+  func getImage() -> UIImage {
+    switch self {
+    case .alarm:
+      return UIImage(systemName: "bell")!
+        .withConfiguration(UIImage.SymbolConfiguration(weight: .regular))
+    case .review:
+      return UIImage(systemName: "questionmark.circle")!
+        .withConfiguration(UIImage.SymbolConfiguration(weight: .regular))
+    case .version:
+      return UIImage(systemName: "exclamationmark.circle")!
+        .withConfiguration(UIImage.SymbolConfiguration(weight: .regular))
+    case .license:
+      return UIImage(systemName: "pencil")!
+        .withConfiguration(UIImage.SymbolConfiguration(weight: .regular))
+    }
+  }
+}
 
 struct Info {
   var title: String
+  var key: InfoKey
 }
