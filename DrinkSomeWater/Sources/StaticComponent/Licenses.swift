c3da08b0a3c7e7a92bd68d367df105d6d2797196
s-doris <swdoriz@gmail.com>
2021-04-16 00:28:18 +0900

    feat : LicensesViewController data binding

---
+++ DrinkSomeWater/Sources/StaticComponent/Licenses.swift
@@ -0,0 +1,26 @@
+//
+//  Licenses.swift
+//  DrinkSomeWater
+//
+//  Created by once on 2021/04/16.
+//
+
+import Foundation
+
+struct Liceses {
+  var library: String
+  
+  static func getData() -> [String] {
+    return ["FSCalendar",
+            "Then",
+            "SnapKit",
+            "RxSwift",
+            "RxCocoa",
+            "RxDataSources",
+            "RxOptional",
+            "RxViewController",
+            "ReactorKit",
+            "WaveAnimationView",
+            "URLNavigator"]
+  }
+}
--- DrinkSomeWater/Sources/ViewController/Licenses/LicensesViewController.swift
+++ DrinkSomeWater/Sources/ViewController/Licenses/LicensesViewController.swift
@@ -5,4 +5,41 @@
 //  Created by once on 2021/04/15.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class LicensesViewController: BaseViewController {
+  
+  
+  // MARK: -UI
+  
+  let licenseList = UITableView().then {
+    $0.register(LicenseCell.self, forCellReuseIdentifier: LicenseCell.cellID)
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.bind()
+  }
+  
+  
+  // MARK: - TableView bind
+  
+  private func bind() {
+    let libraryOb: Observable<[String]> = Observable.of(Liceses.getData())
+    libraryOb
+      .bind(to: licenseList.rx.items(cellIdentifier: LicenseCell.cellID)) {
+        (index: Int, library: String, cell: LicenseCell) in
+        cell.library.text = library
+      }
+      .disposed(by: self.disposeBag)
+  }
+  
+  override func setupConstraints() {
+    self.view.addSubview(licenseList)
+    self.licenseList.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
+}
