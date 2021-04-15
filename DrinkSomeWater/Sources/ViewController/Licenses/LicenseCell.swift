edadd9bb3e4bc9637a59f5c035aa65ae3dfcb56c
s-doris <swdoriz@gmail.com>
2021-04-16 00:20:45 +0900

    feat : LicenseCell init

---
+++ DrinkSomeWater/Sources/ViewController/Licenses/LicenseCell.swift
@@ -0,0 +1,30 @@
+//
+//  LicenseCell.swift
+//  DrinkSomeWater
+//
+//  Created by once on 2021/04/15.
+//
+
+import UIKit
+
+final class LicenseCell: BaseTableViewCell {
+  static let cellID = "LicenseCellID"
+  
+  // MARK: - UI
+  
+  let library = UILabel().then {
+    $0.textColor = .black
+  }
+  
+  override func initialize() {
+    self.backgroundColor = .white
+  }
+  
+  override func setupConstraints() {
+    self.contentView.addSubview(library)
+    self.library.snp.makeConstraints {
+      $0.leading.equalToSuperview().offset(10)
+      $0.centerY.equalToSuperview()
+    }
+  }
+}
---
+++ DrinkSomeWater/Sources/ViewController/Licenses/LicensesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LicensesViewController.swift
+//  DrinkSomeWater
+//
+//  Created by once on 2021/04/15.
+//
+
+import Foundation
