de0df1575984c3e24d3e5581ee045cd5042b044a
s-doris <swdoriz@gmail.com>
2021-04-16 01:26:19 +0900

    feat : infoLabel init

--- DrinkSomeWater/Sources/ViewController/Information/InformationViewController.swift
+++ DrinkSomeWater/Sources/ViewController/Information/InformationViewController.swift
@@ -27,11 +27,16 @@ final class InformationViewController: BaseViewController, View {
   
   // MARK: - UI
   
+  let infoLabel = UILabel().then {
+    $0.text = "설정"
+    $0.textColor = #colorLiteral(red: 0.1739570114, green: 0.1739570114, blue: 0.1739570114, alpha: 1)
+    $0.font = .systemFont(ofSize: 20, weight: .semibold)
+  }
   let backgroundView = UIView().then {
     $0.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
     $0.isUserInteractionEnabled = false
   }
-  
+
   let containerView = UIView().then {
     $0.backgroundColor = .red
     $0.layer.shadowColor = UIColor.black.cgColor
@@ -39,7 +44,6 @@ final class InformationViewController: BaseViewController, View {
     $0.layer.shadowRadius = 10
     $0.layer.shadowOpacity = 0.5
   }
-  
   let tableView = IntrinsicTableView().then {
     $0.register(InfoCell.self, forCellReuseIdentifier: InfoCell.cellID)
     $0.layer.cornerRadius = 20
@@ -49,7 +53,6 @@ final class InformationViewController: BaseViewController, View {
     $0.separatorColor = .clear
     $0.rowHeight = 60
   }
-  
   let backButton = UIButton().then {
     $0.tintColor = .black
     $0.setImage(UIImage(systemName: "arrow.left")?
@@ -137,9 +140,10 @@ final class InformationViewController: BaseViewController, View {
   
   override func setupConstraints() {
     self.view.backgroundColor = .white
-    [self.backButton, self.backgroundView, self.containerView, self.tableView]
+    [self.infoLabel, self.backButton, self.backgroundView, self.containerView, self.tableView]
       .forEach { self.view.addSubview($0) }
-    [self.backButton, self.tableView].forEach { self.view.bringSubviewToFront($0) }
+    [self.infoLabel, self.backButton, self.tableView]
+      .forEach { self.view.bringSubviewToFront($0) }
 
     self.backgroundView.snp.makeConstraints {
       $0.top.leading.trailing.equalToSuperview()
@@ -149,6 +153,10 @@ final class InformationViewController: BaseViewController, View {
       $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(10)
       $0.leading.equalToSuperview().offset(10)
       $0.width.height.equalTo(50)
+    }
+    self.infoLabel.snp.makeConstraints {
+      $0.bottom.equalTo(self.tableView.snp.top).offset(-20)
+      $0.centerX.equalToSuperview()
     }
     self.tableView.snp.makeConstraints {
       $0.top.equalTo(self.view.snp.top).offset(UIScreen.main.bounds.height / 4 - 40)
