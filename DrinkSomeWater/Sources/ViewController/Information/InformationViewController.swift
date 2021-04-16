979db7260ecfdb925d74e74249cfc0249d58bff8
s-doris <swdoriz@gmail.com>
2021-04-16 16:01:01 +0900

    refactor : InfomationView itemselect event 분기 수정

--- DrinkSomeWater/Sources/ViewController/Information/InformationViewController.swift
+++ DrinkSomeWater/Sources/ViewController/Information/InformationViewController.swift
@@ -123,16 +123,15 @@ final class InformationViewController: BaseViewController, View {
     self.tableView.rx.itemSelected
       .subscribe(onNext: { [weak self] indexPath in
         guard let `self` = self else { return }
-        self.tableView.deselectRow(at: indexPath, animated: false)
-        if indexPath.row == 0 {
+        switch indexPath.row {
+        case 0:
           if let bundleIdentifier = Bundle.main.bundleIdentifier,
              let appSettings = URL(string: UIApplication.openSettingsURLString + bundleIdentifier) {
             if UIApplication.shared.canOpenURL(appSettings) {
               UIApplication.shared.open(appSettings)
             }
           }
-        }
-        if indexPath.row == 4 {
+        case 4:
           let vc = LicensesViewController()
           let transition = CATransition()
           transition.duration = 0.4
@@ -141,6 +140,8 @@ final class InformationViewController: BaseViewController, View {
           self.view.window?.layer.add(transition, forKey: kCATransition)
           vc.modalPresentationStyle = .fullScreen
           self.present(vc, animated: false, completion: nil)
+        default:
+          break
         }
       })
       .disposed(by: self.disposeBag)
