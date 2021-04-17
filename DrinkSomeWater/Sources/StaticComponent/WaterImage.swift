7a4021a77baa3cc4ed79e35a22fedfe155d6471d
s-doris <swdoriz@gmail.com>
2021-04-18 02:41:55 +0900

    feat : 물 Percentage 별 이미지 함수 구현

---
+++ DrinkSomeWater/Sources/StaticComponent/WaterImage.swift
@@ -0,0 +1,25 @@
+//
+//  WaterImage.swift
+//  DrinkSomeWater
+//
+//  Created by once on 2021/04/18.
+//
+
+import UIKit
+
+struct WaterImage {
+  static func waterImage(progress: Float) -> UIImage {
+    var image: UIImage?
+    switch progress {
+    case 0..<0.3:
+      image = UIImage(named: "bang3")
+    case 0.3..<0.6:
+      image = UIImage(named: "bang2")
+    case 0.6...1.0:
+      image = UIImage(named: "bang")
+    default:
+      image = UIImage(named: "bang")
+    }
+    return image ?? UIImage()
+  }
+}
