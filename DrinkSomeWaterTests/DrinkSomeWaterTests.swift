8c9017a959e7a53d56305db03ba5f3fbf519978c
s-doris <swdoriz@gmail.com>
2021-05-14 00:20:59 +0900

    feat : test drink water incereseWater & decereseWater init

--- DrinkSomeWaterTests/DrinkSomeWaterTests.swift
+++ DrinkSomeWaterTests/DrinkSomeWaterTests.swift
@@ -15,7 +15,30 @@ class DrinkSomeWaterTests: XCTestCase {
   func testDrinkWaterFetch() {
     let provider = ServiceProvider()
     let reactor = DrinkViewReactor(provider: provider)
-    XCTAssertEqual(reactor.initialState.progress, 0)
+    XCTAssertEqual(reactor.currentState.progress, 0, "failed progress data check")
+    XCTAssertEqual(reactor.currentState.current, 150, "failed current data check")
+    XCTAssertEqual(reactor.currentState.total, 500, "failed total data check")
   }
+  
+  func testDrinkWaterIncerese() {
+    let provider = ServiceProvider()
+    let reactor = DrinkViewReactor(provider: provider)
 
+    reactor.action.onNext(.increseWater)
+    XCTAssertEqual(reactor.currentState.current, 200)
+
+    reactor.action.onNext(.increseWater)
+    XCTAssertEqual(reactor.currentState.current, 250)
+  }
+  
+  func testDrinkWaterDecerese() {
+    let provider = ServiceProvider()
+    let reactor = DrinkViewReactor(provider: provider)
+
+    reactor.action.onNext(.decreseWater)
+    XCTAssertEqual(reactor.currentState.current, 100)
+
+    reactor.action.onNext(.decreseWater)
+    XCTAssertEqual(reactor.currentState.current, 50)
+  }
 }
