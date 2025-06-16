3c7807648515898e5a0c9e558d4242d0aa46f1f7
feelsodev <swdoriz@gmail.com>
2025-06-17 01:55:03 +0900

    chore : 프로젝트 마이그레이션 준비

---
+++ DrinkSomeWater/DrinkSomeWater/Sources/DrinkSomeWaterApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct DrinkSomeWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrinkSomeWater/DrinkSomeWater/Tests/DrinkSomeWaterTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class DrinkSomeWaterTests: XCTestCase {
+    func test_twoPlusTwo_isFour() {
+        XCTAssertEqual(2+2, 4)
+    }
+}
