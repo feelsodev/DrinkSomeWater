57c4d34ea6a9ba705449106285cd684fc7d2c356
feelsodev <swdoriz@gmail.com>
2026-01-04 12:12:05 +0900

    🔧 chore: 다크모드 강제 비활성화 설정
    - Info.plist에 UIUserInterfaceStyle = Light 추가
    - BaseViewController에 overrideUserInterfaceStyle = .light 설정

--- DrinkSomeWater/Sources/ViewController/BaseComponent/BaseViewController.swift
+++ DrinkSomeWater/Sources/ViewController/BaseComponent/BaseViewController.swift
@@ -18,6 +18,7 @@ class BaseViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
         view.backgroundColor = .white
         setupConstraints()
     }
