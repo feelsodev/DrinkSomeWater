6b7d63cda5a11af707ed174671563a669d7c5e5e
feelsodev <swdoriz@gmail.com>
2026-01-27 14:48:56 +0900

    feat(models): add DrinkType model with hydration factors

--- ios/DrinkSomeWater/Sources/Models/WaterRecord.swift
+++ ios/DrinkSomeWater/Sources/Models/WaterRecord.swift
@@ -8,39 +8,55 @@
 import Foundation
 
 struct WaterRecord: ModelType, Identifiable {
- var id: String { date.dateToString }
- var date: Date
- var value: Int
- var isSuccess: Bool
- var goal: Int
- 
- init(date: Date, value: Int, isSuccess: Bool, goal: Int) {
-  self.date = date
-  self.value = value
-  self.isSuccess = isSuccess
-  self.goal = goal
- }
- 
- init?(dictionary: [String: Any]) {
-  guard let date = dictionary["date"] as? Date,
-        let value = dictionary["value"] as? Int,
-        let isSuccess = dictionary["isSuccess"] as? Bool,
-        let goal = dictionary["goal"] as? Int else {
-   return nil
+  var id: String { date.dateToString }
+  var date: Date
+  var value: Int
+  var isSuccess: Bool
+  var goal: Int
+  var drinkType: DrinkType?
+  
+  init(date: Date, value: Int, isSuccess: Bool, goal: Int, drinkType: DrinkType? = nil) {
+   self.date = date
+   self.value = value
+   self.isSuccess = isSuccess
+   self.goal = goal
+   self.drinkType = drinkType
   }
-  self.date = date
-  self.value = value
-  self.isSuccess = isSuccess
-  self.goal = goal
- }
  
- func asDictionary() -> [String: Any] {
-  let dictionary: [String: Any] = [
-   "date": self.date,
-   "value": self.value,
-   "isSuccess": self.isSuccess,
-   "goal": self.goal
-  ]
-  return dictionary
- }
+  init?(dictionary: [String: Any]) {
+   guard let date = dictionary["date"] as? Date,
+         let value = dictionary["value"] as? Int,
+         let isSuccess = dictionary["isSuccess"] as? Bool,
+         let goal = dictionary["goal"] as? Int else {
+    return nil
+   }
+   self.date = date
+   self.value = value
+   self.isSuccess = isSuccess
+   self.goal = goal
+   
+   if let drinkTypeString = dictionary["drinkType"] as? String {
+    self.drinkType = DrinkType(rawValue: drinkTypeString)
+   } else {
+    self.drinkType = nil
+   }
+  }
+ 
+  func asDictionary() -> [String: Any] {
+   var dictionary: [String: Any] = [
+    "date": self.date,
+    "value": self.value,
+    "isSuccess": self.isSuccess,
+    "goal": self.goal
+   ]
+   if let drinkType = drinkType {
+    dictionary["drinkType"] = drinkType.rawValue
+   }
+   return dictionary
+  }
+  
+  var effectiveValue: Int {
+    let type = drinkType ?? .water
+    return type.effectiveAmount(for: value)
+  }
 }
